565b94c1aa0ff36f1455d7eaba2e3040f85589f0
mkaczorow <95172522+mkaczorow@users.noreply.github.com>
2024-07-15 00:13:00 +0200

    poprawienie funkcji do ekstrakcji cech

--- features.py
+++ features.py
@@ -2,6 +2,15 @@ from python_speech_features import logfbank
 import os
 import pickle
 import soundfile as sf
+import numpy as np
+import librosa
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import tensorflow as tf
+
+import crepe
+import tensorflow
 
 #Functions for saving files
 def save_file(filename, data):
@@ -13,7 +22,6 @@ def open_file(filename):
     with open(filename, 'rb') as handle:
         return pickle.load(handle)
 
-#path = "C:\\Users\\magda\\Desktop\\praktyki\\projekt\\TIMIT\\data\\TRAIN\\"
 
 def find_audio_files(directory):
     """
@@ -25,36 +33,48 @@ def find_audio_files(directory):
     audio_files = []
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.lower().endswith('.wav'):
+            if file.lower().endswith('.wav.wav'):
                 audio_files.append(os.path.join(root, file))
     return audio_files
 
-#audio_files = find_audio_files(path)
 
 def read_audio_file(file_path):
     """
     Reads an audio file.
 
     :param file_path: Path of audio file.
-    :return: Tuple (sample_rate, audio_data)
+    :return: tuple (sample_rate, audio_data)
     """
     audio_data, sample_rate = sf.read(file_path, dtype='int16')
     return sample_rate, audio_data
 
-def extract_logfbank_features(audio_files):
+def extract_logfbank_features(name):
     """
-    Extracts features (logfbank) from list of audio files.
+    Extracts features (logfbank) from audio file.
 
-    :param audio_files: List of auio files.
+    :param name: path of auio files.
     :return: List of logfbank features.
     """
-    fbank_feat = []
-    for filename in audio_files:
-        rate, sig = read_audio_file(filename)
-        fbank_f = logfbank(sig, rate)
-        fbank_feat.append(fbank_f)
+    rate, sig = read_audio_file(name)
+    fbank_f = logfbank(sig, rate)
+    fbank_f = (fbank_f - np.mean(fbank_f, axis=0)) / np.std(fbank_f, axis=0)
 
-    return fbank_feat
+    return fbank_f
 
-#fbank_test = extract_logfbank_features(audio_files)
-#print(fbank_test)
+def extract_f0(name):
+    """
+    Extracts features (f0) from audio file.
+
+    :param name: path of audio files.
+    :return: Value of fundamental frequency (mean)
+    """
+    y, sr = librosa.load(name, sr=16000)
+    #frequency = crepe.predict(y, sr, viterbi=True)
+
+    f0, voiced_flag, voiced_probs = librosa.pyin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'), sr=16000)
+    indx = [i for i, vf in enumerate(voiced_flag) if vf]
+
+    frequency = np.mean(f0[indx])
+    frequency = round(frequency,2)
+    return frequency
+
